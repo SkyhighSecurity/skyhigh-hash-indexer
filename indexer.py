761f73230ca81813aebbb85c2d6d95d91424a051
Nate Brady <nate@ultimus.cloud>
2024-05-02 07:21:02 -0500

    add azure

--- indexer.py
+++ indexer.py
@@ -1,14 +1,15 @@
 import yaml
+import requests
+from msal import ConfidentialClientApplication
 import boto3
 from botocore.exceptions import NoCredentialsError, PartialCredentialsError
 import csv
-
 
 def load_config(filename):
     with open(filename, 'r') as file:
         return yaml.safe_load(file)
 
-def connect_to_s3(access_key, secret_access_key):
+def connect_s3(access_key, secret_access_key):
     try:
         return boto3.client(
             's3',
@@ -19,14 +20,14 @@ def connect_to_s3(access_key, secret_access_key):
         print(f"Error connecting to AWS: {e}")
         return None
 
-def list_or_specific_buckets(s3, scan_all, specific_buckets):
+def bucket_list(s3, scan_all, specific_buckets):
     if scan_all:
         response = s3.list_buckets()
         return [bucket['Name'] for bucket in response['Buckets']]
     else:
         return specific_buckets
 
-def gather_bucket_objects(s3, buckets):
+def list_s3_objects(s3, buckets):
     object_details = []
     for bucket in buckets:
         try:
@@ -43,8 +44,7 @@ def gather_bucket_objects(s3, buckets):
             print(f"Error accessing bucket {bucket}: {e}")
     return object_details
 
-
-def write_to_output_bucket(s3, bucket_name, file_key, data):
+def write_object_list(s3, bucket_name, file_key, data):
     try:
         # Prepare CSV data
         csv_content = "ETag,Bucket,Key\n"
@@ -57,19 +57,70 @@ def write_to_output_bucket(s3, bucket_name, file_key, data):
     except Exception as e:
         print(f"Error writing to bucket: {e}")
 
+def authenticate(config):
+    client_id = config['inputs']['Microsoft']['client_id']
+    client_secret = config['inputs']['Microsoft']['client_secret']
+    tenant_id = config['inputs']['Microsoft']['tenant_id']
+    authority_url = f"https://login.microsoftonline.com/{tenant_id}"
+    app = ConfidentialClientApplication(
+        client_id,
+        authority=authority_url,
+        client_credential=client_secret
+    )
+    token_response = app.acquire_token_for_client(scopes=["https://graph.microsoft.com/.default"])
+    return token_response.get('access_token', None)
+
+def get_users_drive_items(graph_access_token):
+    headers = {
+        'Authorization': 'Bearer ' + graph_access_token,
+        'Content-Type': 'application/json'
+    }
+    users_endpoint = "https://graph.microsoft.com/v1.0/users"
+    users_response = requests.get(users_endpoint, headers=headers).json()
+    
+    object_details = []
+    
+    for user in users_response.get('value', []):
+        drive_endpoint = f"https://graph.microsoft.com/v1.0/users/{user['id']}/drive/root/children"
+        drive_response = requests.get(drive_endpoint, headers=headers).json()
+        
+        for item in drive_response.get('value', []):
+            if 'file' in item:
+                object_details.append({
+                    'User': user['displayName'],
+                    'File Name': item['name'],
+                    'File Hash': item.get('file', {}).get('hashes', {}).get('quickXorHash', '')
+                })
+
+    return object_details
+
+def write_drive_items(data):
+    with open('onedrive_files.csv', mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['User', 'File Name', 'File Hash'])
+        for item in data:
+            writer.writerow([item['User'], item['File Name'], item['File Hash']])
+
 def process_accounts(config):
     all_objects = []
     output = config['outputs']['AWS']
-    s3_output = connect_to_s3(output['accessKey'], output['secretAccessKey'])
+    s3_output = connect_s3(output['accessKey'], output['secretAccessKey'])
     
     for account in config['inputs']['AWS']['awsAccounts']:
-        s3 = connect_to_s3(account['accessKey'], account['secretAccessKey'])
+        s3 = connect_s3(account['accessKey'], account['secretAccessKey'])
         if s3:
-            buckets = list_or_specific_buckets(s3, account['scanAllBuckets'], account['buckets'])
-            objects = gather_bucket_objects(s3, buckets)
+            buckets = bucket_list(s3, account['scanAllBuckets'], account['buckets'])
+            objects = list_s3_objects(s3, buckets)
             all_objects.extend(objects)
     
-    write_to_output_bucket(s3_output, output['bucketName'], output['fileKey'], all_objects)
+    write_object_list(s3_output, output['bucketName'], output['fileKey'], all_objects)
+
+    graph_token = authenticate(config)
+    if graph_token:
+        items = get_users_drive_items(graph_token)
+        write_drive_items(items)
+    else:
+        print("Failed to authenticate with Microsoft Graph API.")
 
 if __name__ == "__main__":
     config_path = 'config.yaml'
